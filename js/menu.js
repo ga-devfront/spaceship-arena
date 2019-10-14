002e1112d5e6fc4e2d180a4c65db561ccd02b07a
ga-devfront <oodevilangeloo@gmail.com>
2019-10-14 13:26:03 +0200

    rollback

--- js/menu.js
+++ js/menu.js
@@ -1,13 +1,13 @@
-let audio = document.getElementById('audio');
-let img = document.getElementById('mute_img');
+let audio = document.getElementById("audio");
+let img = document.getElementById("mute_img");
 
 function muteAudio () {
-    if (audio.muted == false) {
-        document.getElementById('audio').muted = true;
-        img.setAttribute('src', "img/nosound.png");
+    if (audio.muted === false) {
+        document.getElementById("audio").muted = true;
+        img.setAttribute("src", "img/nosound.png");
     }
     else {
         document.getElementById('audio').muted = false;
-        img.setAttribute('src', "img/sound.png");
+        img.setAttribute("src", "img/sound.png");
     }
 }
