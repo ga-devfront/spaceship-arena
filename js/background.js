b455f54f504eb91ab1c20d1d2abf4a8564cf8936
ga-devfront <oodevilangeloo@gmail.com>
2019-10-17 16:08:25 +0200

    ajust background

--- js/background.js
+++ js/background.js
@@ -5,15 +5,13 @@ function newImg(myID, source, myLeft, myTop, myIndex) {
     document.body.appendChild(newEl);
     newEl.style.position = "absolute";
     newEl.style.zIndex = myIndex;
-
+    var middlElX = newEl.offsetWidth / 2;
+    var middlElY = newEl.offsetHeight / 2;
+    let calcElX = document.body.offsetWidth * (myLeft - middlElX)  / 100;
+    let calcElY = document.body.offsetHeight * (myTop - middlElY) / 100;
     document.body.addEventListener("mousemove", function(e) {
-        let ow = newEl.offsetWidth;
-        let oh = newEl.offsetHeight;
-        let calcElX = document.body.offsetWidth * myLeft / 100;
-        let calcElY = document.body.offsetHeight * myTop / 100;
-        newEl.style.left = e.screenX / 50 + calcElX + "px";
-        newEl.style.top = e.screenY / 50 + calcElY + "px";
-
+        newEl.style.left = e.screenX / 50 + calcElX  + "px";
+        newEl.style.top = e.screenY / 50 + calcElY  + "px";
     });
 
 
@@ -23,7 +21,7 @@ function newImg(myID, source, myLeft, myTop, myIndex) {
 newImg("lune", "img/lune.png", "70", "50", "1");
 newImg("mars", "img/mars.png", "80", "10", "2");
 newImg("sat", "img/sat.png", "5", "5", "3");
-newImg("star1", "img/star1.png", "90", "35", "4");
-newImg("star2", "img/star2.png", "25", "40", "5");
+newImg("star1", "img/star1.png", "90", "30", "4");
+newImg("star2", "img/star2.png", "25", "35", "5");
 newImg("star3", "img/star3.png", "35", "80", "6");
 newImg("star4", "img/star4.png", "3", "25", "7");
