699cab13d7830e8dd480f088c41aeb3b57dede9b
ga-devfront <oodevilangeloo@gmail.com>
2019-10-17 09:37:17 +0200

    fixe background sound and add button sound hover

--- js/menu.js
+++ js/menu.js
@@ -1,4 +1,5 @@
-let backgroundSound = new Audio("audio/pulsation.mp3"); //select audio background
+let backgroundSoundSource = "audio/pulsation.mp3";
+let backgroundSound = new Audio(); //select audio background
 let buttonSound = new Audio("audio/btn.mp3"); //select audio click button
 
 let step = 0; //initialise les step à 0
@@ -13,6 +14,9 @@ function newButton(emplacement, myID, myClass, myClass2) {
     newEl.setAttribute("id", myID);
     newEl.classList.add(myClass, myClass2);
     emplacement.appendChild(newEl);
+    newEl.addEventListener("mouseover", function(){
+        buttonSound.play();
+    })
 } //function for new element
 
 function newImg(emplacement, source, alternative, myClass) {
@@ -59,9 +63,12 @@ if (step == 0) {
         let imgSound = document.querySelector("#sound_volume img");
         backgroundSound.volume = 0.5;
         
-        backgroundSound.addEventListener("loaded", () => {
+        backgroundSound.addEventListener("load", function() {
             backgroundSound.play();
-          });
+        }, true);
+        backgroundSound.src = backgroundSoundSource;
+        backgroundSound.autoplay = true;
+        backgroundSound.loop = true;
 
           sound_volume.addEventListener('click', function() {
             if (backgroundSound.volume === 0.5) {
