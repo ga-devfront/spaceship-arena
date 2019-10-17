56fdd8e669d988b6f188914ee4d718a78088da9d
ga-devfront <oodevilangeloo@gmail.com>
2019-10-17 09:18:46 +0200

    step0 50%

--- js/menu.js
+++ js/menu.js
@@ -1,4 +1,4 @@
-let background_sound = new Audio("audio/pulsation.mp3"); //select audio background
+let backgroundSound = new Audio("audio/pulsation.mp3"); //select audio background
 let buttonSound = new Audio("audio/btn.mp3"); //select audio click button
 
 let step = 0; //initialise les step à 0
@@ -8,24 +8,25 @@ let header = document.getElementById("header");
 let app = document.getElementById("app");
 let footer = document.getElementById("footer");
 
-function newEl(emplacement, type) {
-    const newEl = document.createElement(type);
+function newButton(emplacement, myID, myClass, myClass2) {
+    const newEl = document.createElement("button");
+    newEl.setAttribute("id", myID);
+    newEl.classList.add(myClass, myClass2);
     emplacement.appendChild(newEl);
-} //création et placement d'un nouvel élément
+} //function for new element
 
-function newElement(emplacement, type, attribute, valeurAttribute) {
-    const newEl = document.createElement(type);
-    newEl.setAttribute(attribute, valeurAttribute);
-    newEl.classList.add("opacitytest");
+function newImg(emplacement, source, alternative, myClass) {
+    const newEl = document.createElement("img");
+    newEl.setAttribute("src", source);
+    newEl.setAttribute("alt", alternative);
+    newEl.classList.add(myClass);
     emplacement.appendChild(newEl);
-    setTimeout(appear, 500);
-    function appear (){
-        if (newEl.style.opacity == 0) {
-            newEl.style.opacity = "1";
-            clearTimeout;
-        }
-    }
-}
+} //function for new image
+
+function newText(emplacement, myText) {
+    const newEl = document.createTextNode(myText);
+    emplacement.appendChild(newEl);
+} //function for new element
 
 
 function supress(selection) {
@@ -35,11 +36,46 @@ function supress(selection) {
 } //supression du contenue
 
 
-
 if (step == 0) {
     supress(header);
     supress(app);
     supress(footer);
-    newElement(header, "img", "src", "img/logo.png");
-    newElement(footer, "img", "src", "img/fb.png")
-}
+
+        newImg(header, "img/logo.png", "logo SpaceShip Arena", "none");
+
+        newButton(app, "start", "large", "big-font");
+        newText(start, "Start", "none");
+
+        newButton(footer, "insta", "small", "marg-lr10");
+        newImg(insta, "img/insta.png", "symbole instagram", "none");
+        newButton(footer, "fb", "small", "nmarg-lr10");
+        newImg(fb, "img/fb.png", "symbole facebook", "none");
+        newButton(footer, "twitter", "small", "marg-lr10");
+        newImg(twitter, "img/twitter.png", "symbole twitter", "none");
+        newButton(footer, "rules", "large", "marg-lr10");
+        newText(rules, "Game rules");
+        newButton(footer, "sound_volume", "nostyle", "marg-lr10");
+        newImg(sound_volume, "img/sound.png", "contrôle du son", "none");
+        let imgSound = document.querySelector("#sound_volume img");
+        backgroundSound.volume = 0.5;
+        
+        backgroundSound.addEventListener("loaded", () => {
+            backgroundSound.play();
+          });
+
+          sound_volume.addEventListener('click', function() {
+            if (backgroundSound.volume === 0.5) {
+                backgroundSound.volume = 0.2;
+                imgSound.setAttribute("src", "img/middlesound.png");
+            } //turne volume to 0.2
+            else if (backgroundSound.volume === 0.2) {
+                backgroundSound.volume = 0;
+                imgSound.setAttribute("src", "img/nosound.png");
+            } //turne volume off
+            else {
+                backgroundSound.volume = 0.5;
+                imgSound.setAttribute("src", "img/sound.png");
+            } //turne volume on
+        });
+    
+    }
